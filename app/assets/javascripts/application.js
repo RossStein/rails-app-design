b830f6c97e253d6fcbf8f1e537dfb5f9ac612365
T Ross Steinhilber <Ross@Ts-MacBook-Pro.attlocal.net>
2016-02-22 23:32:53 -0600

    header and sidebar styling

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,38 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+$(document).ready(function () {
+	$('.sidebar-toggle .fa-comment').click(function(event) {
+		if ( $('.main-content').hasClass('merge-left') ) {
+			$('.right-sidebar').toggleClass('open');
+			$('.main-content').toggleClass('shift-left');
+			$('.main-content').toggleClass('merge-left');
+			// $('.left-sidebar').toggleClass('hide-left');
+		}
+		else {
+			$('.left-sidebar').toggleClass('hide-left');
+			$('.main-content').toggleClass('shift-left');
+			$('.right-sidebar').toggleClass('open');
+		}
+	});
+	$('.sidebar-toggle .fa-bars').click(function(event) {
+		if ( $('.right-sidebar').hasClass('open') ) {
+			$('.right-sidebar').removeClass('open');
+			$('.left-sidebar').toggleClass('hide-left');
+			$('.main-content').toggleClass('shift-left');
+		}
+		else {
+			$('.left-sidebar').toggleClass('hide-left');
+			$('.main-content').toggleClass('merge-left');
+		}
+	});
+	$('.main-content').click(function(event) {
+		if ( $('.right-sidebar').hasClass('open') ) {
+		     $('.right-sidebar').removeClass('open');
+		     $('.left-sidebar').removeClass('hide-left');
+		     $('.main-content').removeClass('shift-left');
+		}
+	});
+})
+
